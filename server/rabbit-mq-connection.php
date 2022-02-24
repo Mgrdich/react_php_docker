3154c4d8fcd5a227d20a0f4814d4b548232a8ba9
Mgrdich <mgotm13@gmail.com>
2022-02-24 23:28:43 +0400

    rabbitMQ finished

--- server/rabbit-mq-connection.php
+++ server/rabbit-mq-connection.php
@@ -1,19 +1,26 @@
 <?php
 
-echo "RabbitMq connection";
 
 
 $rabbitmq_host = "rabbit-mq";
-$rabbitmq_port = 5671;
-$rabbitmq_user = 'rabbitmq_user';
-$rabbitmq_pass = 'rabbitmq_pass';
+$rabbitmq_port = 5672;
+/* $rabbitmq_user = 'rabbitmq_user'; */
+/* $rabbitmq_pass = 'rabbitmq_pass'; */
+$rabbitmq_user = 'guest';
+$rabbitmq_pass = 'guest';
+$rabbitmq_vhost = '/';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Connection\AMQPSSLConnection;
 use PhpAmqpLib\Message\AMQPMessage;
+try {
+    $connection = new AMQPSSLConnection($rabbitmq_host ,$rabbitmq_port, $rabbitmq_user, $rabbitmq_pass, $rabbitmq_vhost);
 
-$connection = new AMQPSSLConnection($rabbitmq_host ,$rabbitmq_port, $rabbitmq_user, $rabbitmq_pass, $vhost);
+    $channel = $connection->channel();
 
-$channel = $connection->channel();
-
-var_dump($channel);
+    if(!empty($channel)) {
+        echo "RabbitMq Connected Successfully";
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
