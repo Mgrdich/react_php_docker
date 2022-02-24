f32731aa79b4590dcaa968e7e97f18d6db380929
Mgrdich <mgotm13@gmail.com>
2022-02-24 20:48:54 +0400

    fix mysql db connection

--- server/db-connection.php
+++ server/db-connection.php
@@ -1,12 +1,12 @@
 <?php
 
-$servername = "localhost";
+$servername = "mysql-db";
 $username = "mysql-user";
 $password = "mysql-password";
 $db = "mysql-db";
 
     try {
-       $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+       $conn = new PDO("mysql:host=$servername;dbname=$db;port=3306", $username, $password);
        // set the PDO error mode to exception
        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
        echo "Connected successfully";
